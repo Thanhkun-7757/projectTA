346e503c70c1d5f263ce29811a9979acb8356907
Nguyen Ba Thanh <nguyenbathanh20122000@gmail.com>
2025-02-11 21:30:01 +0700

    firt commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,27 @@
+namespace WinFormsApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,27 @@
+namespace WinFormsApp
+{
+    partial class Form2
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form2";
+            this.ResumeLayout(false);
+        }
+    }
+}
